25fc4d84c4c83dbb05872faf9ce32a4fa0812ff0
Bokyung-R <100823426+Bokyung-R@users.noreply.github.com>
2023-04-06 21:48:20 +0900

    2주차 과제
    2주차 과제 문제 4개

---
+++ main2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main() {
+	int N = 0;
+	map<int,int> map;
+	int value = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> value;
+		map[value] = i;
+	}
+
+	int M = 0;
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> value;
+		cout << map.count(value) << " ";
+	}
+	cout << endl;
+
+}
+//시간초과 + count가 0과 1로만 뜸
